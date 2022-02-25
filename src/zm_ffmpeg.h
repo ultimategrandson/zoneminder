403059467753a3399233215800f71d0fe9add70e
Ultimategrandson <douglasmarttinen@gmail.com>
2022-02-25 11:24:55 +1030

    more

--- src/zm_ffmpeg.h
+++ src/zm_ffmpeg.h
@@ -133,7 +133,8 @@ extern "C" {
  * b and c the minor and micro versions of libav
  * d and e the minor and micro versions of FFmpeg */
 #define LIBAVCODEC_VERSION_CHECK(a, b, c, d, e) \
-    ( (LIBAVCODEC_VERSION_MICRO <  100 && LIBAVCODEC_VERSION_INT >= AV_VERSION_INT(a, b, c) ) || \
+    ( (a == 57 && LIBAVCODEC_VERSION_MAJOR == 58) || 
+      (LIBAVCODEC_VERSION_MICRO <  100 && LIBAVCODEC_VERSION_INT >= AV_VERSION_INT(a, b, c) ) || \
       (LIBAVCODEC_VERSION_MICRO >= 100 && LIBAVCODEC_VERSION_INT >= AV_VERSION_INT(a, d, e) ) )
 
 #elif HAVE_FFMPEG_AVCODEC_H
